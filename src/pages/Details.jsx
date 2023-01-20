5ada40fb9b073f93ac87a090e6878ab2fbeb5475
Michael2000-kpi <85711932+Michael2000-kpi@users.noreply.github.com>
2023-01-21 01:31:40 +0200

    world nation map
    world nation map

---
+++ src/pages/Details.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry} from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
+import {ALL_COUNTRIES} from '../config';
+export const Details = () => {
+  const { name } = useParams();
+
+  const navigate = useNavigate();
+  const [countries,setCountries]=useState(null);
+  
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data})=>setCountries(data[0]));
+
+  },[name])
+  
+  return (
+    <div>
+    
+      <Button onClick={() => navigate(-1)}><IoArrowBack />Back</Button>
+      {countries && <Info {...countries} />}
+     
+    </div>
+  );
+};
